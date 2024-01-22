525be482833af0a32ddf864dd5301693e4168834
alec-tackitt-student <144161418+alec-tackitt-student@users.noreply.github.com>
2024-01-22 13:14:38 -0500

    Create program.cs

---
+++ program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome.");
+        Console.WriteLine("Press enter to start.");
+        Console.ReadLine();
+        while(true)
+        {
+            Console.WriteLine("Press control+c to stop this loop.");
+        }
+        Console.WriteLine("Game over.");
+    }
+}
