adbd105cddc61bcf96dc9ea6a390745d2b74a8a2
Max Baykowski <maba24e@oaitc.net>
2024-01-22 14:34:10 -0500

    Began working on player class

--- Player.cs
+++ Player.cs
@@ -1,1 +1,30 @@
-
+public class Player
+{
+    private int health;
+    private string name;
+    private int heals;
+    private int maxHeals=3;
+    public Player(string name)
+    {
+        this.name=name;
+    }
+    public string GetName()
+    {
+        return name;
+    }
+    public void SetHealth(int number)
+    {
+        if(number <= 0)
+        {
+            Console.WriteLine("You must enter a positive number.");
+        }
+        else
+        {
+            health = number;
+        }
+    }
+    public int GetHealth()
+    {
+        return health;
+    }
+}
