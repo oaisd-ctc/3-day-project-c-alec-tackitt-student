23da9d99bc11e20a03192aa5996592e3d58ba204
Max Baykowski <maba24e@oaitc.net>
2024-01-22 13:16:28 -0500

    Added program.cs

--- program.cs
+++ program.cs
@@ -1,15 +1,2 @@
-﻿using System;
-public class Program
-{
-    public static void Main(string[] args)
-    {
-        Console.WriteLine("Welcome.");
-        Console.WriteLine("Press enter to start.");
-        Console.ReadLine();
-        while(true)
-        {
-            Console.WriteLine("Press control+c to stop this loop.");
-        }
-        Console.WriteLine("Game over.");
-    }
-}
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
