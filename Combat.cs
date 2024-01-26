78a4405259bc7e1173a94f26263fa809e568c440
alec-tackitt-student <144161418+alec-tackitt-student@users.noreply.github.com>
2024-01-26 18:28:07 +0000

    push to share again

--- Combat.cs
+++ Combat.cs
@@ -1,16 +1,15 @@
 
 public static class Combat
 {
-     Random rnd = new Random();
-    int num = rnd.Next();
 
+   
     public static int TakeDamage()
     {
-     return rnd.Next(1,16);
+     return new Random().Next(1,16);
     }
     public static int Heal()
     {
-        return Rnd.Next(5,10);
+        return new Random().Next(5,10);
     }
     public static bool block()
     {
