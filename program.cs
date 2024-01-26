97be118c3200682de340bd10c623fe2c66b8ae7e
alec-tackitt-student <144161418+alec-tackitt-student@users.noreply.github.com>
2024-01-26 17:02:36 +0000

    push to share problems

--- program.cs
+++ program.cs
@@ -9,19 +9,61 @@ public class Program
         Console.WriteLine("Press enter to start.");
         System.Console.WriteLine("");
         System.Console.WriteLine("enter Player 1's name:");
-        string player1Name = Console.ReadLine();
+        string player1Name = Console.ReadLine()?;
         System.Console.WriteLine("enter Player 2's name:");
-        string player2Name = Console.ReadLine();
-
-        Player player1 = new Player(player1Name);
-        Player player2 = new Player(player2Name);
+        string player2Name = Console.ReadLine()?:;
+        Player :player1 = new Player(player1Name);
+        Player :player2 = new Player(player2Name);
 
         System.Console.WriteLine($"cool names {player1Name} and {player2Name}, good luck and have fun!");
 
         while(true)
         {
-            Console.WriteLine("Press control+c to stop this loop.");
+            Player1Selection(player1Name);
+            Player2Selection(player2Name);
+            CheckForDeath();
+
+            
         }
         Console.WriteLine("Game over.");
     }
+     public static void Player1Selection(string player1Name){
+            Console.WriteLine($"{player1Name} sleect what you want to do:");
+            System.Console.WriteLine("1. Attack");
+            System.Console.WriteLine("2. Block");
+            System.Console.WriteLine("3. Heal");
+            int player1Choice = int.parse(Console.ReadLine()!);
+            
+            switch (player1Choice){
+                case 1:
+                    //attack method
+                    break;
+                case 2:
+                    //block method
+                case 3:
+                    //heal method
+                    break;
+            }
+        }
+        public static void Player2Selection(string player2Name){
+            Console.WriteLine($"{player1Name} sleect what you want to do:");
+            System.Console.WriteLine("1. Attack");
+            System.Console.WriteLine("2. Block");
+            System.Console.WriteLine("3. Heal");
+            int player2Choice = int.parse(Console.ReadLine()!);
+            
+            switch (player2Choice){
+                case 1:
+                    //attack method
+                    break;
+                case 2:
+                    //block method
+                case 3:
+                    //heal method
+                    break;
+            }
+        }
+        public static void CheckForDeath(){
+
+        }
 }
